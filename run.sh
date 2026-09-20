9d1ec8db372e888736854c606dff0c3a7ec78489
Dima Bukharin <111676756+Dimekadze@users.noreply.github.com>
2026-09-21 00:12:51 +0500

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 set -e
 
-REPO_URL="git@github.com:Dimekadze/VolumeHandChanger.git"
 PROJECT_DIR="VolumeHandChanger"
 PYTHON_VERSION="python3.12"
 ENTRY_POINT="src/volumehandchanger/VolumeControl.py"
@@ -9,10 +8,6 @@ ENTRY_POINT="src/volumehandchanger/VolumeControl.py"
 if ! command -v poetry &>/dev/null; then
     echo "Poetry not installed"
     exit 1
-fi
-
-if [ ! -d "$PROJECT_DIR" ]; then
-    git clone "$REPO_URL"
 fi
 
 cd "$PROJECT_DIR"
